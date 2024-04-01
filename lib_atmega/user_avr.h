0d56bef8da6549a47e5738067135999529f3b112
yel1919 <eliseev-kolya-yel@yandex.ru>
2024-04-02 00:31:38 +0500

    first commit

---
+++ lib_atmega/user_avr.h
@@ -0,0 +1,65 @@
+#ifndef USER_AVR
+#define USER_AVR
+
+#define PNA0 		0x10	//16
+#define PNA1 		0x11	//17
+#define PNA2 		0x12	//18
+#define PNA3 		0x13	//19
+#define PNA4 		0x14	//20
+#define PNA5 		0x15	//21
+#define PNA6 		0x16	//22
+#define PNA7 		0x17	//23
+
+#define PNB0 		0x20	//32
+#define PNB1 		0x21	//33
+#define PNB2 		0x22	//34
+#define PNB3 		0x23	//35
+#define PNB4 		0x24	//36
+#define PNB5 		0x25	//37
+#define PNB6 		0x26	//38
+#define PNB7 		0x27	//39
+
+#define PNC0 		0x30	//48
+#define PNC1 		0x31	//49
+#define PNC2 		0x32	//50
+#define PNC3 		0x33	//51
+#define PNC4 		0x34	//52
+#define PNC5 		0x35	//53
+#define PNC6 		0x36	//54
+#define PNC7 		0x37	//55
+
+#define PND0 		0x40	//64
+#define PND1 		0x41	//65
+#define PND2 		0x42	//66
+#define PND3 		0x43	//67
+#define PND4 		0x44	//68
+#define PND5 		0x45	//69
+#define PND6 		0x46	//70
+#define PND7 		0x47	//71
+
+#define PNE0 		0x50	//80
+#define PNE1 		0x51	//81
+#define PNE2 		0x52	//82
+#define PNE3 		0x53	//83
+#define PNE4 		0x54	//84
+#define PNE5 		0x55	//85
+#define PNE6 		0x56	//86
+#define PNE7 		0x57	//87
+
+#define PNF0 		0x60	//96
+#define PNF1 		0x61	//97
+#define PNF2 		0x62	//98
+#define PNF3 		0x63	//99
+#define PNF4 		0x64	//100
+#define PNF5 		0x65	//101
+#define PNF6 		0x66	//102
+#define PNF7 		0x67	//103
+
+#define PNG0 		0x70	//112
+#define PNG1 		0x71	//113
+#define PNG2 		0x72	//114
+#define PNG3 		0x73	//115
+#define PNG4 		0x74	//116
+#define PNG5 		0x75	//117
+
+#endif //!USER_AVR
